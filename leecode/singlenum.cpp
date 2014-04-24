dec9534526a18099f46e097865f160ffce4e56f7
chunfu <hideoryan@gmail.com>
2014-04-24 16:18:47 +0800

    XOR to implement single num problem

--- leecode/singlenum.cpp
+++ leecode/singlenum.cpp
@@ -6,17 +6,22 @@ using namespace std;
 class Solution {
 	public:
 		int singleNumber(int A[], int n) {
+			int single = 0;
 			if(n % 2 == 0)
 				return -1;
+			for(int i = 0; i < n; i++) {
+				single = single ^ A[i];
+			}
+			return single;
 		}
 };
 
 
 int main() {
 	int n = 9;
-	int A[n];
+	int A[] = {1,1,3,4,5,6,4,6,3};
 	Solution *sol = new Solution();
-	sol->singleNumber();
+	cout << sol->singleNumber(A, n) << endl;
 
 	return 0;
 }
